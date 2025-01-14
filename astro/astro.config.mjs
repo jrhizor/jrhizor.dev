efedcfe2b0e4914b5114e6a9b6461c61bb382615
jrhizor <me@jaredrhizor.com>
2025-01-14 01:29:57 -0800

    prettier

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jrhizor.dev',
-    integrations: [mdx(), sitemap(), react(), tailwind()],
+  site: "https://jrhizor.dev",
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 });
