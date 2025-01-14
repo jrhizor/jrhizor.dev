1901df0e49be079ef52854144a79fc7fa4aa3fc4
jrhizor <me@jaredrhizor.com>
2025-01-14 04:21:36 -0800

    more natural bolding

--- next/src/app/projects/page.tsx
+++ next/src/app/projects/page.tsx
@@ -1,31 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "~/components/ui/card"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export default function ProjectsPage() {
-    return (
-        <main>
-            <h1 className="text-3xl md:text-4xl leading-tight font-light">Projects</h1>
-            <h2 className="uppercase text-sm text-neutral-500 font-normal">Current Focus</h2>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>Card Content</p>
-                </CardContent>
-                <CardFooter>
-                    <p>Card Footer</p>
-                </CardFooter>
-            </Card>
-        </main>
-    );
+  return (
+    <main>
+      <h1 className="text-3xl font-light leading-tight md:text-4xl">
+        Projects
+      </h1>
+      <h2 className="text-sm font-normal uppercase text-neutral-500">
+        Current Focus
+      </h2>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
 }
